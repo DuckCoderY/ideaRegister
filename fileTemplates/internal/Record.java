c60279524d6e751fa077c247b0758d240b5e1528
init_ly <init_ly@foxmail.com>
2022-05-24 11:06:45 +0800

    IU-2020.2.4 <admin_liZ@DESKTOP-7DEU6AJ Create baseRefactoring.xml, markdown.xml, ide.general.xml, mybatisx.xml, project.default.xml, rainbow_brackets.xml, diff.xml, Enum.java, Interface.java, lombok-plugin.xml, yiiguxing.translation.xml, shared-indexes.xml, Default.xml, find.xml, ui.lnf.xml, gradle.settings.xml, filetypes.xml, lermitage-extra-icons.xml, remote-servers.xml, xsl.xml, androidEditors.xml, colors.scheme.xml, KeyPromoterXSettings.xml, databaseDrivers.xml, templates.xml, Record.java, GrepConsole.xml, Default.xml, laf.xml, docker.xml, Class.java, jdk.table.xml, path.macros.xml, lightEdit.xml, mavenRunHelper.xml, wsl.distributions.xml, vcs.xml, codota.xml, toStringTemplates.xml, editor.xml, ChartsConfig.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Record.java
@@ -0,0 +1,12 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+ * @ClassName: ${NAME}
+ * @Date: ${DATE} ${HOUR}:${MINUTE} 
+ * @Author: l_y
+ * @Version: 1.0
+ */
+ 
+public record ${NAME}(#[[$END$]]#) {
+}
